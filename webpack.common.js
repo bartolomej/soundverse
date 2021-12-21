4782040f0fcc3b16ca1c557eedf9242445e906b4
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-12-22 00:13:40 +0100

    init project

---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const SOURCE_ROOT = path.resolve(__dirname, 'src');
+const DIST_ROOT = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    new CopyPlugin([
+      {
+        from: path.join(SOURCE_ROOT, 'index.html'),
+        to: DIST_ROOT
+      },
+      {
+        from: path.join(SOURCE_ROOT, 'assets'),
+        to: DIST_ROOT
+      },
+    ]),
+  ],
+  output: {
+    path: DIST_ROOT,
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+const DIST_ROOT = path.resolve(__dirname, 'dist');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: DIST_ROOT,
+    hot: true,
+    stats: 'minimal'
+  },
+});
