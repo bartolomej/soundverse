8cce2faeace367cb95e55dfdff487d4ee65b6938
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-01-05 18:50:48 +0100

    add sound fade effect

--- src/app/Speaker.ts
+++ src/app/Speaker.ts
@@ -1,4 +1,4 @@
-import {Howl, Howler} from "howler";
+import { Howl, Howler } from "howler";
 import { vec3 } from "gl-matrix";
 import { Track } from "./Music";
 
@@ -11,6 +11,7 @@ export default class Speaker {
   private playlist: Howl[];
   private currentIndex: number;
   private autoplay: boolean;
+  private fadeDuration = 2000;
 
   constructor (position: vec3, autoplay = true) {
     this.position = position;
@@ -18,11 +19,11 @@ export default class Speaker {
     this.playlist = [];
   }
 
-  getCurrentTrack() {
+  getCurrentTrack () {
     return this.playlist[this.currentIndex];
   }
 
-  setPlaylist(list: Track[]) {
+  setPlaylist (list: Track[]) {
     this.playlist = list.map((track) => {
       const sound = new Howl({
         src: [track.preview],
@@ -31,14 +32,14 @@ export default class Speaker {
         onplay: () => this.onTrackPlay(track),
       });
       // set speaker position
-      const [x,y,z] = this.position;
+      const [x, y, z] = this.position;
       sound.pos(x, y, z);
       return sound;
     });
     this.currentIndex = 0;
   }
 
-  onTrackEnd(track: Track) {
+  onTrackEnd (track: Track) {
     console.log("Track ended: ", track.title);
     if (this.currentIndex <= this.playlist.length - 1) {
       this.currentIndex++;
@@ -50,25 +51,33 @@ export default class Speaker {
     }
   }
 
-  onTrackPlay(track: Track) {
+  onTrackPlay (track: Track) {
     console.log("Track started: ", track.title);
+    const currentTrack = this.getCurrentTrack();
+    const endDuration = currentTrack.duration() - currentTrack.seek();
+    setTimeout(() => {
+        currentTrack.fade(1, 0, this.fadeDuration)
+      },
+      endDuration * 1000 - this.fadeDuration
+    );
   }
 
-  play() {
+  play () {
     const track = this.getCurrentTrack();
-   track?.load();
+    track?.load();
     // play on load, if track isn't loaded yet
-   track?.on("load", () => {
-     track?.play();
+    track?.on("load", () => {
+      track?.play();
+      track?.fade(0, 1, this.fadeDuration)
     })
   }
 
-  pause() {
+  pause () {
     this.getCurrentTrack()?.pause();
   }
 
-  setPlayerPosition(playerPosition: vec3) {
-    const [x,y,z] = playerPosition;
+  setPlayerPosition (playerPosition: vec3) {
+    const [x, y, z] = playerPosition;
     Howler.pos(x, y, z);
   }
 
