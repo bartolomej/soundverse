9b9ebdb6da2bbf0db39370fd80c8ebaf73c4e644
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2022-01-06 14:29:44 +0100

    track cross-fade, prefetch sound files

--- src/app/Speaker.ts
+++ src/app/Speaker.ts
@@ -8,28 +8,37 @@ import { Track } from "./Music";
 export default class Speaker {
 
   private readonly position: vec3; // speaker position
-  private playlist: Howl[];
+  private tracks: Howl[];
   private currentIndex: number;
   private autoplay: boolean;
-  private fadeDuration = 2000;
+  private fadeDuration = 4000;
 
   constructor (position: vec3, autoplay = true) {
     this.position = position;
     this.autoplay = autoplay;
-    this.playlist = [];
+    this.tracks = [];
   }
 
   getCurrentTrack () {
-    return this.playlist[this.currentIndex];
+    return this.tracks[this.currentIndex];
+  }
+
+  getNextTrack () {
+    return this.tracks[this.currentIndex + 1];
+  }
+
+  prefetch() {
+    this.tracks.forEach(track => track.load());
   }
 
   setPlaylist (list: Track[]) {
-    this.playlist = list.map((track) => {
+    this.tracks = list.map((track) => {
       const sound = new Howl({
         src: [track.preview],
         preload: false,
         onend: () => this.onTrackEnd(track),
         onplay: () => this.onTrackPlay(track),
+        onload: () => console.log("Track loaded: ", track.title)
       });
       // set speaker position
       const [x, y, z] = this.position;
@@ -41,35 +50,63 @@ export default class Speaker {
 
   onTrackEnd (track: Track) {
     console.log("Track ended: ", track.title);
-    if (this.currentIndex <= this.playlist.length - 1) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0;
-    }
-    if (this.autoplay) {
-      this.play();
-    }
   }
 
   onTrackPlay (track: Track) {
     console.log("Track started: ", track.title);
     const currentTrack = this.getCurrentTrack();
     const endDuration = currentTrack.duration() - currentTrack.seek();
-    setTimeout(() => {
-        currentTrack.fade(1, 0, this.fadeDuration)
-      },
+    setTimeout(this.onTrackFadeStart.bind(this),
       endDuration * 1000 - this.fadeDuration
     );
   }
 
+  onTrackFadeStart() {
+    console.log("Track fade started");
+    this.playNext();
+  }
+
+  playNext() {
+    const currentTrack = this.getCurrentTrack();
+    currentTrack.fade(1, 0, this.fadeDuration)
+    if (this.currentIndex <= this.tracks.length - 1) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+    this.play();
+  }
+
+  playPrevious() {
+    const currentTrack = this.getCurrentTrack();
+    currentTrack.fade(1, 0, this.fadeDuration)
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.tracks.length - 1;
+    }
+    this.play();
+  }
+
   play () {
     const track = this.getCurrentTrack();
-    track?.load();
-    // play on load, if track isn't loaded yet
-    track?.on("load", () => {
-      track?.play();
+    const playTrack = (track: Howl) => {
+      if (!track.playing()) {
+        track?.play();
+      }
       track?.fade(0, 1, this.fadeDuration)
-    })
+    };
+    if (track.state() === "unloaded") {
+      track?.load();
+    }
+    if (track.state() === "loading") {
+      // play on load, if track isn't loaded yet
+      track?.on("load", () => playTrack(track))
+    }
+    if (track.state() === "loaded") {
+      playTrack(track)
+    }
+    this.getNextTrack()?.load();
   }
 
   pause () {
